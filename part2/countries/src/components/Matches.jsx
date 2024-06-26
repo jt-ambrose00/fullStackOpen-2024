ed91e13431a629b46cdbe141f293d0120079d960
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-06-25 23:59:31 -0500

    finished 2.20

--- part2/countries/src/components/Matches.jsx
+++ part2/countries/src/components/Matches.jsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from 'react';
+
+import searchService from '../services/search';
+
 const Matches = ({ searchedCountry, showCountry }) => {
-    if (searchedCountry.length > 10) {
-        return <p>too many matches, specify another filter</p>;
-    };
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        if (searchedCountry.length === 1) {
+          const country = searchedCountry[0];
+          searchService
+            .getWeather(country)
+            .then(weatherData => setWeather(weatherData));
+        };
+      }, [searchedCountry]);
 
     if (searchedCountry.length === 1) {
         const country = searchedCountry[0];
         const languages = Object.values(country.languages);
+        const temperature = weather ? weather.main.temp - 273.15 : null;
+        const weatherIcon = weather ? `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png` : null;
 
         return (
             <div key={country.cca3}>
@@ -23,8 +36,23 @@ const Matches = ({ searchedCountry, showCountry }) => {
                     alt={country.flags.alt}
                     className="flag-image"
                 />
+                {weather && (
+                    <>
+                        <h3>Weather in {country.capital}</h3>
+                        <p>temperature: {temperature.toFixed(2)} celsius</p>
+                        <img
+                            src={weatherIcon}
+                            alt={weather.weather[0].description}
+                        />
+                        <p>wind: {weather.wind.speed} m/s</p>
+                    </>
+                )}
             </div>
         );
+    };
+
+    if (searchedCountry.length > 10) {
+        return <p>too many matches, specify another filter</p>;
     };
 
     return (
--- part2/countries/src/services/search.js
+++ part2/countries/src/services/search.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
+
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all';
+const weatherBaseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const weatherApi = import.meta.env.VITE_WEATHER_API;
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
+  const request = axios.get(baseUrl);
   return request.then(response => response.data)
 };
 
-export default { getAll };
+const getWeather = (country) => {
+  const request = axios.get(
+    `${weatherBaseUrl}?q=${country.capital},${country.cca2}&APPID=${weatherApi}`
+  );
+  return request.then(response => response.data)
+};
+
+export default { getAll, getWeather };
