9e64cc76173589b6fc62bcd1c76e8cdb3a11cb3f
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-07-21 10:00:06 -0500

    finished 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,50 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const Blog = require('../models/blog')
+const app = require('../app')
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+      title: 'React Patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7
+    },
+    {
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+        likes: 0
+    },
+    {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[2])
+    await blogObject.save()
+})
+
+test('correct number of blogs returned as json', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
