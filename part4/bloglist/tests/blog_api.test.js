07300d1afe6e40c2e2788faf6a5eb55664453016
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-07-26 12:32:48 -0500

    finished 4.23

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -2,10 +2,14 @@ const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const app = require('../app')
 const api = supertest(app)
 
+let token
 const initialBlogs = [
     {
       title: 'React Patterns',
@@ -29,12 +33,28 @@ const initialBlogs = [
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('salainen', 10)
+    let initialUser = new User({
+        username: 'mluukkai',
+        name: 'Matti Luukkainen',
+        passwordHash
+    })
+    await initialUser.save()
+
+    let blogObject = new Blog({ ...initialBlogs[0], user: initialUser._id })
     await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
+    blogObject = new Blog({ ...initialBlogs[1], user: initialUser._id })
     await blogObject.save()
-    blogObject = new Blog(initialBlogs[2])
+    blogObject = new Blog({ ...initialBlogs[2], user: initialUser._id })
     await blogObject.save()
+
+    const userForToken = {
+        username: initialUser.username,
+        id: initialUser._id,
+    }
+    token = jwt.sign(userForToken, process.env.SECRET)
 })
 
 test('correct number of blogs returned as json', async () => {
@@ -55,7 +75,7 @@ test('verify that unique identifier is named id', async () => {
     })
 })
 
-test('a valid blog can be added ', async () => {
+test('a valid blog can be added', async () => {
     const newBlog = {
         title: 'First class tests',
         author: 'Robert C. Martin',
@@ -65,6 +85,7 @@ test('a valid blog can be added ', async () => {
 
     await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token}`)
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
@@ -85,6 +106,7 @@ test('a blog can be added without specifying likes', async () => {
 
     await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token}`)
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
@@ -108,12 +130,14 @@ test('blog won\'t be added without title/url', async () => {
 
     await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token}`)
         .send(noTitle)
         .expect(400)
         .expect('Content-Type', /application\/json/)
 
     await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token}`)
         .send(noUrl)
         .expect(400)
         .expect('Content-Type', /application\/json/)
@@ -125,6 +149,7 @@ test('a blog can be deleted', async () => {
 
     await api
         .delete(`/api/blogs/${blogToDelete.id}`)
+        .set('Authorization', `Bearer ${token}`)
         .expect(204)
 
     const endingBlogs = await api.get('/api/blogs')
@@ -149,6 +174,26 @@ test('a blog can be updated', async () => {
     const updatedBlog = endingBlogs.body[2]
 
     assert.strictEqual(updatedBlog.likes, initialBlogs[2].likes + 1)
+})
+
+test('blog can\'t be added without a valid token', async () => {
+    const newBlog = {
+        title: 'First class tests',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+        likes: 10
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(401)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+
+    assert(!titles.includes('First class tests'))
 })
 
 after(async () => {
