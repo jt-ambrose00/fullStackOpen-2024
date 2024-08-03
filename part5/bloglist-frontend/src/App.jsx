bb51dbabe14f950395e5c8985904a3f0ad4cad7d
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-08-03 04:06:10 -0500

    finished 5.9/5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -150,9 +150,11 @@ const App = () => {
         <BlogForm createBlog={createBlog} />
       </Togglable>
       <br />
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
+        )}
     </>
   )
 }
