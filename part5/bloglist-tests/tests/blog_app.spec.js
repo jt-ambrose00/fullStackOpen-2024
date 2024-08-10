2d44522022cde18e6910de424170b5a91837627d
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-08-10 14:39:18 -0500

    finished 5.18

--- part5/bloglist-tests/tests/blog_app.spec.js
+++ part5/bloglist-tests/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:5173/api/testing/reset')
+    await request.post('http://localhost:5173/api/users', {
+      data: {
+        username: 'mluukkai',
+        name: 'Matti Luukkainen',
+        password: 'salainen'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -13,5 +22,27 @@ describe('Blog app', () => {
     await expect(loginButton).toBeVisible()
     await expect(usernameInput).toBeVisible()
     await expect(passwordInput).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+        await page.getByTestId('username').fill('mluukkai')
+        await page.getByTestId('password').fill('salainen')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('Matti Luukkainen logged-in')).toBeVisible()
+        await expect(page.getByRole('button', { name: 'logout' })).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+        await page.getByTestId('username').fill('mluukkai')
+        await page.getByTestId('password').fill('cinnamon')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('invalid username or password'))
+            .toBeVisible()
+        await expect(page.getByText('Matti Luukkainen logged-in'))
+            .not.toBeVisible()
+    })
   })
 })
